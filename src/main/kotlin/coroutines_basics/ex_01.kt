33282f96e72834e1b8f3c7b20d4b538e195adb8d
조경현 <jkh9615@naver.com>
2022-04-03 17:56:02 +0900

    gitIgnore 적용

---
+++ src/main/kotlin/coroutines_basics/ex_01.kt
@@ -0,0 +1,13 @@
+package coroutines_basics
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking{
+    launch{
+        delay(1000)
+        println("World!")
+    }
+    println("Hello")
+}
