3c9af7b0c2bbb65823ebbe1a45296b1aaaabf55f
조경현 <jkh9615@naver.com>
2022-04-03 18:15:03 +0900

    An explict job

---
+++ src/main/kotlin/coroutines_basics/ex_05.kt
@@ -0,0 +1,16 @@
+package coroutines_basics_05
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val job = launch {
+        delay(1000)
+        println("World!")
+    }
+
+    println("Hello")
+    job.join() // 자식 코루틴(job)이 완료될 때까지 대기
+    println("DONE")
+}
